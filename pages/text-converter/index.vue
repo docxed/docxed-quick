23595e549b1b2c923298d3768415fcba2acd7db4
docxed <akira.ajeyb@gmail.com>
2025-01-21 10:10:43 +0700

    refactor text replacement logic and enhance UI for replace list management

--- pages/text-converter/index.vue
+++ pages/text-converter/index.vue
@@ -8,13 +8,15 @@ const textToLower = useLocalStorage('text-converter-text-to-lower', '')
 
 // text to replace
 const textToReplace = useLocalStorage('text-converter-text-to-replace', '')
-const replaceList = useLocalStorage('text-converter-replace-list', '')
+const replaceList = useLocalStorage<{ find: string; replace: string }[]>(
+  'text-converter-replace-list',
+  [],
+)
 const resultReplace = computed(() => {
   try {
-    let list = replaceList.value.split(',').map((item) => item.split(':'))
     let result = textToReplace.value
-    list.forEach((item) => {
-      result = result.replace(new RegExp(item[0].trim(), 'g'), item[1].trim())
+    replaceList.value.forEach((replace) => {
+      result = result.replaceAll(replace.find, replace.replace)
     })
     return result
   } catch (err) {
@@ -56,15 +58,33 @@ const resultReplace = computed(() => {
           clearable
           autogrow
         />
-        <q-input
-          v-model="replaceList"
-          type="textarea"
-          label="Replace list"
-          filled
-          clearable
-          autogrow
-          hint="format array: key1: value1, key2: value2"
+        <q-icon
+          name="add"
+          class="cursor-pointer"
+          @click="replaceList.push({ find: '', replace: '' })"
+          color="primary"
+          size="sm"
         />
+        <div
+          class="row q-gutter-x-xs"
+          v-for="(replace, index) in replaceList"
+          :key="`replace-${index}`"
+        >
+          <div class="col">
+            <q-input v-model="replace.find" label="Find" filled />
+          </div>
+          <div class="col">
+            <q-input v-model="replace.replace" label="Replace" filled>
+              <template v-slot:append>
+                <q-icon
+                  name="delete"
+                  class="cursor-pointer"
+                  @click="replaceList.splice(replaceList.indexOf(replace), 1)"
+                />
+              </template>
+            </q-input>
+          </div>
+        </div>
         <q-input
           :model-value="resultReplace"
           type="textarea"
